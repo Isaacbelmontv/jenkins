836dcb9f6cc84f9b8b62cc43c4cf621e0889aa5a
ibelmont <ibelmont@dentalia.com>
2019-11-22 14:46:12 -0600

    primer commit

---
+++ src/main/java/com/dentalia/dentaliaproject/DentaliaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.dentalia.dentaliaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DentaliaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DentaliaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dentalia/dentaliaproject/DentaliaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dentalia.dentaliaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DentaliaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
